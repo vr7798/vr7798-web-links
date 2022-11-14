d8110015a7c17b6ca6238e4addf619801e802e44
vr7798 <vr_77982hotmail.com>
2022-11-14 16:40:09 -0400

    tt

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = 8081;
 
 app.get("/", function (rep, res) {
   res.sendFile(path.join(__dirname + "/src"));
 });
 
+app.get("/comentarios", function (rep, res) {
+  res.sendFile(path.join(__dirname + "/src/indexc.html"));
+});
+
 app.use(express.static("src"));
 
-app.listen(process.env.PORT, () => {
+app.listen(8081, () => {
   console.log(`servidor rodando na porta ${port}`);
 });
