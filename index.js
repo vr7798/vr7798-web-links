b15ecb837db0cd76b744d1699253f2b246219078
vr7798 <vr_77982hotmail.com>
2022-12-18 12:32:02 -0400

    tt

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ app.get("/", function (rep, res) {
   res.sendFile(path.join(__dirname + "/src"));
 });
 
-app.get("/comentarios", function (rep, res) {
-  res.sendFile(path.join(__dirname + "/src/indexc.html"));
-});
-
 app.use(express.static("src"));
 
 app.listen(PORT, () => {
